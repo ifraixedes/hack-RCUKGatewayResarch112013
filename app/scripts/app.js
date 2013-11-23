401167603a1f9ddb4b452005e224a28ec8f0d95c
Seyi Ogunyemi <micrypt@gmail.com>
2013-11-23 21:26:23 +0000

    scripts/app: stub out test calls against API.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,17 +2,26 @@
     var path = window.location.pathname.split('/');
     var f = {
         'person': function(id){
-            console.log(id);
+            GtR.persons(id, function(data) {
+                console.log(data);
+            });
+            GtR.personsOrganisations(id, function(data) {
+                console.log(data);
+            });
         },
         'organisation': function(id){
-            console.log(id);
+            GtR.organisations(id, function(data) {
+                console.log(data);
+            });
         },
-        'publication': function(id){
+        'project': function(id){
             console.log(id);
         }
     };
     if(path.length>2) {
         var pageType = path[1];
-        f[pageType].apply(this,path.slice(2,path.length));
+        if(f.hasOwnProperty(pageType)) {
+            f[pageType].apply(this,path.slice(2,path.length));
+        }
     }
 })();
