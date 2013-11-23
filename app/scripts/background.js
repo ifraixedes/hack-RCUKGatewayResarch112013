30ea6a5d45076ad4a700ca2850e89e330f9bda21
Ivan Fraixedes <ifcdev@gmail.com>
2013-11-23 16:24:18 +0000

    I changed the chrome-extension to load a blank page
    Added permissions and script to load a blank page when a development
    url is requested

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,12 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+  return {redirectUrl: chrome.extension.getURL('html/graph.html')};
+}, 
+{
+  urls: ['http://hack.gtr.rcuk.ac.uk/*']
+}, 
+['blocking']);
+
