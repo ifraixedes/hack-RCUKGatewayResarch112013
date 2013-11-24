73f08aa0622c257f43c14b0b2b1dba4260be2f69
Seyi Ogunyemi <micrypt@gmail.com>
2013-11-24 00:06:36 +0000

    scripts/app: switch app to only include get().

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,9 @@
-(function() {
-    var path = window.location.pathname.split('/');
-    var f = {
-        'person': function(id){
-            GtR.persons(id, function(data) {
-                console.log(data);
-            });
-            GtR.personsOrganisations(id, function(data) {
-                console.log(data);
-            });
-        },
-        'organisation': function(id){
-            GtR.organisations(id, function(data) {
-                console.log(data);
-            });
-        },
-        'project': function(id){
-            console.log(id);
-        }
-    };
+function getID() {
+    var path = window.location.pathname.split('/'),
+        id;
     if(path.length>2) {
         var pageType = path[1];
-        if(f.hasOwnProperty(pageType)) {
-            f[pageType].apply(this,path.slice(2,path.length));
-        }
+        id = path[2];
     }
-})();
+    return id;
+};
